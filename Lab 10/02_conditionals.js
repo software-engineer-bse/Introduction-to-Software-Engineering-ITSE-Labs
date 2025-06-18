b258fc57fb4a43270d4eaf1df84c03215f4cfd3e
software-engineer-bse <khurram.email.123@gmail.com>
2025-06-18 13:23:41 -0700

    Add 02_conditionals.js in Lab 10

---
+++ Lab 10/02_conditionals.js
@@ -0,0 +1,13 @@
+// if, else if, else
+let number = 2;
+
+if(number==="2"){
+    console.log("Number is string 2");
+} else if (number === 2){
+    console.log("Number is 2")
+} else {
+    console.log("The number is other then 2")
+}
+
+
+// Write a condition to check even or odd number.
