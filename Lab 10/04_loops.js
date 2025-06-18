80b839f49afaa6341f1734d7da2f20c35a16a982
software-engineer-bse <khurram.email.123@gmail.com>
2025-06-18 13:27:56 -0700

    Add 04_loops.js in Lab 10

---
+++ Lab 10/04_loops.js
@@ -0,0 +1,22 @@
+// for, while loops
+console.log("For Loop Start")
+
+for (let i = 1; i<=10; i++){
+    console.log(i)
+}
+
+console.log("For Loop End")
+
+console.log();
+
+console.log("While Loop Start");
+
+number = 0;
+while(number<10){
+    console.log(number);
+    number++;
+}
+
+console.log("While Loop End");
+
+// Mini Task: Print your name 10 times using any type of loop (for or while loop).
