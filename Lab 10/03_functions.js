bef7dfb2560d69adf784571ff1f1a526b40f3c73
software-engineer-bse <khurram.email.123@gmail.com>
2025-06-18 13:25:36 -0700

    Add 03_functions.js in Lab 10

---
+++ Lab 10/03_functions.js
@@ -0,0 +1,11 @@
+// Functions: reusable code blocks
+function add(a,b){
+    let sum = a + b;
+    return sum;
+}
+
+console.log(add(10,20))
+
+
+// Write a function sayHi(name) that prints "Hi <name>".
+// For example: Hi Khurram
