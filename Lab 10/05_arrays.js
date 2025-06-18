2d143231f993f0a9e0755832873a0cac655efd7e
software-engineer-bse <khurram.email.123@gmail.com>
2025-06-18 13:30:11 -0700

    Add 05_arrays.js in Lab 10

---
+++ Lab 10/05_arrays.js
@@ -0,0 +1,14 @@
+// Arrays: storing multiple values
+// Example List in Python or array in Java
+let food = ["apple", "banana", "mango"];
+
+console.log(food);
+
+console.log(food[0]);
+
+food.push("chai")
+
+console.log(food)
+
+
+// Mini Task: Create an array of 3 programming languages and print that array
