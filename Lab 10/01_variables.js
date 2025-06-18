aa62a6c0e6507f766750e913354128c232a69ddc
software-engineer-bse <khurram.email.123@gmail.com>
2025-06-18 13:21:24 -0700

    Add 01_variables.js in Lab 10

---
+++ Lab 10/01_variables.js
@@ -0,0 +1,11 @@
+// var old JavaScript mai comman tha, magar abhi tak use hota hai.
+// let new JavaScript mai use hota hai.
+// const bhi new JavaScript mai constant k liye use hota hai
+// Primitive types: number, string, boolean, null, undefined
+let name = "Khurram";
+const age = 25;
+var isStudent = true;
+
+console.log(name + " is " + age + " years old.");
+
+// Mini Task: Declare 3 variables: username, score, isLoggedIn. Print them in a sentence.
